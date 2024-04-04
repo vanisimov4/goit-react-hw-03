6fc9c4917d85fa64a9a20a0a90c15768d3774070
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-04-04 23:07:26 +0300

    cssFinish

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,12 +1,33 @@
+import { useId } from 'react';
 import { Formik, Form, Field } from 'formik';
 import css from './ContactForm.module.css';
 
+const initialValues = {
+  id: '',
+  name: '',
+  number: '',
+};
+
 const ContactForm = () => {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <Form className={css.form}>
-        <Field type="text" name="name" />
-        <Field type="number" name="number" />
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" id={nameFieldId} />
+        </div>
+        <div>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field type="text" name="number" id={numberFieldId} />
+        </div>
         <button type="submit">Add contact</button>
       </Form>
     </Formik>
