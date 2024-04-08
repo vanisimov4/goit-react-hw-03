fd48b13d510311cd408eca694b03cbc757098e6d
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-04-08 21:29:28 +0300

    finish

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import ContactForm from '../contactForm/ContactForm';
 import SearchBox from '../searchBox/SearchBox';
@@ -6,12 +6,26 @@ import ContactList from '../contactList/ContactList';
 import './App.css';
 
 function App() {
-  const [contList, setContList] = useState([
+  const initial = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
+
+  const [contList, setContList] = useState(() => {
+    const savedContacts = window.localStorage.getItem('contList');
+
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts);
+    }
+
+    return initial;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('contList', JSON.stringify(contList));
+  }, [contList]);
 
   const [searchValue, setSearchValue] = useState('');
 
@@ -48,26 +62,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React is amazing</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */
-}
